f05d8aed6b7f904ca9d15c31fd7e0b1f50d78933
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2025-10-09 19:42:06 +0530

    first code in js

---
+++ first.js
@@ -0,0 +1,34 @@
+console.log("ishaan varshney");
+let name="rittik";
+let a=null;
+let b=undefined;
+console.log(typeof(a));
+
+// make a object
+
+const student={
+    name:"ishaan",
+    age:20,
+    cgpa:9.5,
+    isPass:true
+};
+
+console.log(student);
+console.log(typeof(student));
+
+console.log(student["name"]);
+console.log(typeof(student["name"]));
+
+student["name"]="ishu";
+console.log(student["name"]);
+
+student.name="ishaan";
+console.log(student.name);
+
+
+
+
+
+
+
+
