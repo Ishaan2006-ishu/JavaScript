7b0ea574bcff4b703eb96c2779ea4b17a0d56668
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2025-10-10 18:09:06 +0530

    contro state

---
+++ operatoe and conditional.js
@@ -0,0 +1,36 @@
+//q1. get user to input a no using promt("enter a no"). check if no is a multiple of 5 or not.
+
+// let number=prompt("enter a no");
+// if(number%5===0){
+//     console.log("true");
+    
+
+// }
+// else{
+//     console.log("false");
+    
+// }
+
+//q2. write a code which can give grades to students according to their scores:
+let grade=prompt("grade of student");
+
+if(grade >=80 && grade<=100 ){
+    console.log('A');
+    
+}
+else if(grade >=70 && grade<80 ){
+    console.log('b');
+    
+}
+else if(grade >=60 && grade<69 ){
+    console.log('c');
+    
+}
+else if(grade >=50 && grade<59 ){
+    console.log('d');
+    
+}
+if(grade >=0 && grade<50 ){
+    console.log('e');
+    
+}
