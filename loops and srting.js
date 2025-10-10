986f7bd73801228f37e23f4280e2b1e6b348d8b3
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2025-10-10 19:10:55 +0530

    loops and str

---
+++ loops and srting.js
@@ -0,0 +1,39 @@
+//q1. print all even no from 0 to 100.
+// for(let i =0;i<=100;i++){
+//     if(i%2===0){
+//         console.log(i);
+        
+//     }
+    
+// }
+
+//q2. make user guess number game.
+
+// let gamenum=25;
+
+
+// let usernum=prompt("Guess the game number :");
+// console.log(typeof(usernum));
+
+// while(usernum != gamenum){
+//     usernum=prompt("you entered wrong num");
+// }
+
+// console.log('contratulation');
+
+
+// templates literal.
+
+// let obj={
+//     pen:"red",
+//     price:10
+// };
+
+// console.log(`pen color ${obj.pen} and pen price is ${obj.price}`);
+
+//q3. prompt the user to enter the full name. and the give the out put in differnt way.
+
+// let username=prompt("enter the name");
+// console.log(`@ ${username} ${username.length}`);
+
+
