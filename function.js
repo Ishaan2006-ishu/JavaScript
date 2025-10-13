1de08b79a5d57857724230da429d79b6d4aaccb7
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2025-10-13 09:03:51 +0530

    function

---
+++ function.js
@@ -0,0 +1,105 @@
+//q1. count no vowel in str
+
+// function count(str){
+//     let sum=0;
+//     for(let i=0;i<str.length;i++){
+//         if (
+//             str[i] ==="a" ||
+//             str[i] ==="e" ||
+//             str[i] ==="i" ||
+//             str[i] ==="o" ||
+//             str[i] ==="u" 
+//         ){
+//             sum+=1;
+//         }
+
+
+
+//     }
+//     console.log('no of vowel =', sum);
+    
+// }
+
+// count("ishaan");
+
+// const Arr_count=(str)=>{
+//     let sum=0;
+//     for(let i=0;i<str.length;i++){
+//         if (
+//             str[i] ==="a" ||
+//             str[i] ==="e" ||
+//             str[i] ==="i" ||
+//             str[i] ==="o" ||
+//             str[i] ==="u" 
+//         ){
+//             sum+=1;
+//         }
+
+
+
+//     }
+//     console.log('no of vowel =', sum);
+    
+// }
+
+// Arr_count("ishaan")
+
+//q2. print the square of numbersusing for each loop.
+
+// nums=[1,2,3,4,5];
+// nums.forEach(num => {
+//     console.log(num*num);
+    
+    
+// });
+
+// map function
+// let nums=[1,2,3,4,5];
+// arr=nums.map((val)=>{
+//     return (val*val);
+    
+// })
+// console.log(arr);
+
+//filter
+
+// let arr=[1,2,3,4,5,6];
+// let new_arr=arr.filter((val)=>{
+//     return val%2===0;
+
+// })
+// console.log(new_arr);
+
+// Reduce Function
+// let arr=[5,3,6,98,8];
+// let new_arr=arr.reduce((res,curr)=>{
+//     //return res+curr;
+//     return res<curr? curr:res;
+// });
+// console.log(new_arr);
+
+// q4. filter the topper
+// let  marks=[91,92,93,45,89];
+// let toppers=marks.filter((val)=>{
+//     return val>90;
+// })
+// console.log(toppers);
+
+// take the input and find the sum
+// let n=prompt("enter a no");
+// let arr=[];
+
+// for(let i=1;i<=n;i++){
+//     arr[i-1]=i
+// }
+// console.log(arr);
+
+// let ans=arr.reduce((res,curr)=>{
+//     return res+curr;
+// });
+
+// console.log(ans);
+
+
+
+
